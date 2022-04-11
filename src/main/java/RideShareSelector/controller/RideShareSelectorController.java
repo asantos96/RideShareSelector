00a99c88b2b73a5bb9b9813379e1797f22923dc8
Andrea Santos <andreasantos@macbook-pro.myfiosgateway.com>
2022-04-11 11:51:12 -0400

    initial repo

---
+++ src/main/java/RideShareSelector/controller/RideShareSelectorController.java
@@ -0,0 +1,9 @@
+package RideShareSelector.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RideShareSelectorController {
+
+}
---
+++ src/main/java/RideShareSelector/service/apiService.java
@@ -0,0 +1,11 @@
+package RideShareSelector.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class apiService {
+
+
+    }
+
