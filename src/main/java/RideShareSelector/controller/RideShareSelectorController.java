c449a7451fee213000f00c5aa0ec9a8e24581c13
RUQAYYAH MUSTAFA <ruqayyahmustafa@RUQAYYAHs-MacBook-Pro.local>
2022-04-12 16:46:53 -0400

    adds html with a dropdown menu of origin and destination addresses. Retruns the two addresses

--- src/main/java/RideShareSelector/controller/RideShareSelectorController.java
+++ src/main/java/RideShareSelector/controller/RideShareSelectorController.java
@@ -1,7 +1,10 @@
 package RideShareSelector.controller;
 import RideShareSelector.model.MapData;
 import RideShareSelector.service.apiService;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import org.springframework.stereotype.Controller;
@@ -9,14 +12,21 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class RideShareSelectorController {
-    //@Autowired
-    //public apiService apiService;
-    @RequestMapping("/")
+
+    @RequestMapping( "/")
+    //@ResponseBody
+    public String loadPage(){
+        return "test1.html";
+    }
+
+    @RequestMapping(value = "/destination", method = RequestMethod.POST)
     @ResponseBody
 
-    public String testMapData() {
+    public String testMapData(@RequestParam("destinationAddress") String destinationAddress,
+                              @RequestParam("originAddress") String originAddress, ModelMap modelMap) {
         apiService mapAPI = new apiService();
         MapData test = mapAPI.accessMapData();
+        //modelMap.put(" ", );
         String currentStatus = test.getStatus();
         String city = test.getOriginAddresses().get(0);
 
@@ -24,7 +34,7 @@ public class RideShareSelectorController {
         String distance = test.getRows().get(0).getElements().get(0).getDistance().getText();
 
 
-        return distance;
+        return "Destination Address: " + destinationAddress + " Origin Address: " + originAddress;
     }
 
 
