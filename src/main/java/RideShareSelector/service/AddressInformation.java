d7879accaad773bf64a2a14b0d18f042e4b450f9
Andrea Santos <andreasantos@macbook-pro.myfiosgateway.com>
2022-04-12 14:47:54 -0400

    Start of parseJson method

--- src/main/java/RideShareSelector/service/AddressInformation.java
+++ src/main/java/RideShareSelector/service/AddressInformation.java
@@ -1,15 +1,26 @@
 package RideShareSelector.service;
 
 public class AddressInformation {
+
     private String originStreetNumber;
     private String originStreetName;
     private String destinationStreetNumber;
     private String getDestinationStreetName;
 
-    public AddressInformation(String originStreetNumber, String originStreetName, String destinationStreetNumber, String getDestinationStreetName) {
-        this.originStreetNumber = originStreetNumber;
-        this.originStreetName = originStreetName;
-        this.destinationStreetNumber = destinationStreetNumber;
-        this.getDestinationStreetName = getDestinationStreetName;
-    }
+//    public String parseJson(String URL) {
+////
+////        return
+//
+
+//    private String originStreetNumber;
+//    private String originStreetName;
+//    private String destinationStreetNumber;
+//    private String getDestinationStreetName;
+//
+//    public AddressInformation(String originStreetNumber, String originStreetName, String destinationStreetNumber, String getDestinationStreetName) {
+//        this.originStreetNumber = originStreetNumber;
+//        this.originStreetName = originStreetName;
+//        this.destinationStreetNumber = destinationStreetNumber;
+//        this.getDestinationStreetName = getDestinationStreetName;
+//    }
 }
--- src/main/java/RideShareSelector/service/apiService.java
+++ src/main/java/RideShareSelector/service/apiService.java
@@ -15,7 +15,8 @@ public class apiService {
         return restTemplate.getForObject("https://maps.googleapis.com/maps/api/distancematrix/json?origins=1%20First%20Avenue%2C%20Baltimore%2C%20MD&destinations=15%20West%20North%20Avenue%2C%20Baltimore%2C%20MD&units=imperial&key=AIzaSyC92D7nxoEuB7-Epx6wBw-iNjdEIELhGkY", MapData.class);
 
 //        AddressInformation addressInformation = new AddressInformation()
-//        return restTemplate.getForObject("https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + streetNumber + "%20" + streetName +  "%20First%20Avenue%2C%20Baltimore%2C%20MD&destinations=15%20West%20North%20Avenue%2C%20Baltimore%2C%20MD&units=imperial&key=AIzaSyC92D7nxoEuB7-Epx6wBw-iNjdEIELhGkY", MapData.class);)
+//        return restTemplate.getForObject("https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + streetNumber +
+//        "%20" + streetName +  "%2C%20" + "Baltimore%2C%20MD&destinations=15%20West%20North%20Avenue%2C%20Baltimore%2C%20MD&units=imperial&key=AIzaSyC92D7nxoEuB7-Epx6wBw-iNjdEIELhGkY", MapData.class);)
     }//end accessMapData
 
 }//end apiService
